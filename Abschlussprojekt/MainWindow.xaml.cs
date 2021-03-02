65d54205ca6579561da36455d3ae821150c33c93
wanwitfe <wanwitfe@IT-PC-01>
2021-03-02 15:27:30 +0100

    Initialisierung

--- Abschlussprojekt/MainWindow.xaml.cs
+++ Abschlussprojekt/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Abschlussprojekt
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
